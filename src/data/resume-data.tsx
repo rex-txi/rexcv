5d010b6fce538312f7695d0f98a9f76b7d6c1cc3
Rex Chekal <108820378+rex-txi@users.noreply.github.com>
2023-12-27 05:57:08 -0600

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,14 +20,14 @@ import { LinkedInIcon } from "../components/icons/LinkedInIcon";
 import { XIcon } from "../components/icons/XIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Rex Chekal",
+  initials: "rexc",
+  location: "Chicago, Il",
+  locationLink: "https://www.google.com/maps/place/chicago",
   about:
-    "Full Stack Engineer focused on building products with extra attention to details",
+    "A multi-disciplinary designer and creative",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
+    "info tk.",
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
